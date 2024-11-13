2877336eb787d3b7b3f06bb095e2083b4190d76a
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2024-11-13 15:49:45 -0500

    espero que ahora sí por favor en serio

--- src/app/doctor-dashboard/page.tsx
+++ src/app/doctor-dashboard/page.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import React, { useEffect } from "react";
-import { preLoaderAnim } from "@/utils/animation";
-import "../../utils/preloader.css";
-
 const DoctorDashboardPage = () => {
-  useEffect(() => {
-    preLoaderAnim();
-  }, []);
   return (
-    <div className="preloader">
-      <div className="texts-container">
-        <span>Bienvenido a AlivIA, </span>
-        <span>Dr. Josue Cordova</span>
-        <span>Gracias por elejir trabajar con nosotros</span>
-      </div>
+    <div className="">
+      <span>Bienvenido a AlivIA, doctor</span>
     </div>
   );
 };
